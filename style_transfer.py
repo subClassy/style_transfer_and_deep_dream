80856184dab4f071ddf067ce0b9d08dc5190986f
Himank Goel <himankgoel03@gmail.com>
2021-09-03 22:14:16 +0530

    transferhub to apply neural transfer

---
+++ style_transfer.py
@@ -0,0 +1,46 @@
+import tensorflow as tf
+import matplotlib.pyplot as plt
+import tensorflow_hub as hub
+import numpy as np
+import PIL.Image
+
+def load_img(path_to_img):
+  max_dim = 512
+  img = tf.io.read_file(path_to_img)
+  img = tf.image.decode_image(img, channels=3)
+  img = tf.image.convert_image_dtype(img, tf.float32)
+
+  shape = tf.cast(tf.shape(img)[:-1], tf.float32)
+  long_dim = max(shape)
+  scale = max_dim / long_dim
+
+  new_shape = tf.cast(shape * scale, tf.int32)
+
+  img = tf.image.resize(img, new_shape)
+  img = img[tf.newaxis, :]
+  return img
+
+def imshow(image):
+  if len(image.shape) > 3:
+    image = tf.squeeze(image, axis=0)
+
+  plt.imshow(image)
+
+def tensor_to_image(tensor):
+  tensor = tensor*255
+  tensor = np.array(tensor, dtype=np.uint8)
+  if np.ndim(tensor)>3:
+    assert tensor.shape[0] == 1
+    tensor = tensor[0]
+  return PIL.Image.fromarray(tensor)
+
+content_image = load_img('./images/t-rex.jpg')
+style_image = load_img('./images/japaneseart.jpg')
+
+hub_module = hub.load('https://tfhub.dev/google/magenta/arbitrary-image-stylization-v1-256/1')
+stylized_image = hub_module(tf.constant(content_image), tf.constant(style_image))[0]
+
+img = tensor_to_image(stylized_image)
+
+plt.imshow(img)
+plt.show()
